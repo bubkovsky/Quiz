35b8e3783036b06899ca36b0c792e8099db6592d
bubkovsky <49303703+bubkovsky@users.noreply.github.com>
2019-09-01 12:30:43 +0200

    Quiz question : added: setters for question and answers; method rotateAnswers method correctAnswers

--- src/main/java/QuizQuestion.java
+++ src/main/java/QuizQuestion.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class QuizQuestion {
     String question;
-    List<String> answers;
+    List<String> answers = new ArrayList<>(  );
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public void setAnswers(String answer) {
+        answers.add( answer );
+    }
+
+    static List<String> rotateAnswers(List<String> answers) {
+        List<String> tempLista = answers;
+        Collections.shuffle( tempLista );
+        return tempLista;
+    }
+    static String getCorrectAnswer(List<String> anwers){
+        return anwers.get( 0 );
+    }
 }
